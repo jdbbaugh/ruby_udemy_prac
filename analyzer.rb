6f5b836e838675664cbe3f4fa25d5d765de532d6
jdbbaugh <jbaugh78@msn.com>
2019-06-01 12:08:24 -0500

    up to video 29

---
+++ analyzer.rb
@@ -0,0 +1,13 @@
+puts "what is your first name"
+first_name = gets.chomp
+puts "what is your last name"
+last_name = gets.chomp
+full_name = "#{first_name} #{last_name}"
+puts "Your full name is #{full_name}"
+puts "Your name reversed is #{full_name.reverse}"
+puts "Your name has #{full_name.length - 1 }"
+
+# puts 'enter a number to mulitply by two'
+# input = gets.chomp
+# puts input.to_i * 2
+
