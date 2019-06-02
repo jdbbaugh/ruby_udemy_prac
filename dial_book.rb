56bebe7faec0333339b877926b2d222cb841d4ea
jdbbaugh <jbaugh78@msn.com>
2019-06-02 14:28:27 -0500

    completed vid44

--- dial_book.rb
+++ dial_book.rb
@@ -13,17 +13,12 @@ dial_book = {
 
 #get city names from the hash
 def get_city_names(somehash)
-  puts "Which city do you want the area code for?"
-  somehash.each do |k, v|
-    puts k
-  end
+  somehash.keys
 end
 
 #get area code based on given hash and key (key is city name with goal being to retun area code number for that city)
 def get_area_code(somehash, key)
-  puts "#{key}'s aread code is #{somehash[key]}"
-  25.times{print "--"}
-  puts
+  somehash[key]
 end
 
 #Execution flow
@@ -31,8 +26,15 @@ loop do
   puts "Do you want to look up an area code based on a city name? (Y/N)"
   answer = gets.chomp.downcase
   break if answer != "y"
-  get_city_names(dial_book)
+  puts "Which city do you want the area code for?"
+  puts get_city_names(dial_book)
   puts "Enter your selection"
   chosen_city = gets.chomp.downcase
-  get_area_code(dial_book, chosen_city)
+  if dial_book.include?(chosen_city)
+    puts "#{chosen_city}'s area code is #{get_area_code(dial_book, chosen_city)}"
+  else
+    puts "You entered an invalid city name."
+  end
+  25.times{print "--"}
+  puts
 end
