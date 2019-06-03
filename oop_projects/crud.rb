3e665a714e95915546a7508e7893168fe7b7318d
jdbbaugh <jbaugh78@msn.com>
2019-06-03 18:50:02 -0500

    user is authenticating

--- oop_projects/crud.rb
+++ oop_projects/crud.rb
@@ -23,4 +23,18 @@ def create_secure_users(list_of_users)
     list_of_users
 end
 
-puts create_secure_users(users)
+new_users = create_secure_users(users)
+puts new_users
+
+def authenticate_user(username, password, list_of_users)
+  list_of_users.each do |user_record|
+    if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+      25.times{print "--"}
+      puts
+      return user_record
+    end
+  end
+  "Credentials were not correct."
+end
+
+p authenticate_user("Phill", "passPhill", new_users)
