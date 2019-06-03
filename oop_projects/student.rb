4d40b8375ee64e678a27cdb1c1fa24f7bc3b0212
jdbbaugh <jbaugh78@msn.com>
2019-06-03 17:22:49 -0500

    completed 49

--- oop_projects/student.rb
+++ oop_projects/student.rb
@@ -1,14 +1,27 @@
 class Student
-  @first_name
-  @last_name
-  @email
-  @username
-  @password
+  attr_accessor :first_name, :last_name, :email, :username, :password
+
+  def initialize(first_name, last_name, email, username, password)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @username = username
+    @password = password
+  end
 
   def to_s
-    "First Name: #{@first_name}"
+    "First Name: #{@first_name}, Last Name: #{@last_name}, Username: #{@username}, Email Address: #{@email}"
+  end
 
 
 end
 
-joey = Student.new
+joey = Student.new("Joey", "Baugh", "jbaugh@me.com", "jdb", "pass")
+jon = Student.new("Jon", "Doe", "jon@msn.com", "jon", "pass2")
+
+puts joey
+puts jon
+
+joey.last_name = jon.last_name
+puts "joey is altered"
+puts joey
